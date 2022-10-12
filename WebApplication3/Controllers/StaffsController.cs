8ed27df34baf4281d78bfac5b2bdef09332a457b
НаумовСА <@105-09>
2022-10-12 13:20:11 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/StaffsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication3.Entities;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class StaffsController : ApiController
+    {
+        private PrimerForApiStaffs db = new PrimerForApiStaffs();
+
+        // GET: api/Staffs
+        [ResponseType(typeof(List<StaffModel>))]
+
+        public IHttpActionResult GetStaff()
+        {
+            return Ok(db.Staff.ToList().ConvertAll(x=> new StaffModel(x))); 
+        }
+
+        // GET: api/Staffs/5
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult GetStaff(int id)
+        {
+            Staff staff = db.Staff.Find(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(staff);
+        }
+
+        // PUT: api/Staffs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStaff(int id, Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != staff.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(staff).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Staffs
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult PostStaff(Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Staff.Add(staff);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = staff.id }, staff);
+        }
+
+        // DELETE: api/Staffs/5
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult DeleteStaff(int id)
+        {
+            Staff staff = db.Staff.Find(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            db.Staff.Remove(staff);
+            db.SaveChanges();
+
+            return Ok(staff);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StaffExists(int id)
+        {
+            return db.Staff.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ WebApplication3/Models/StaffModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication3.Entities;
+
+namespace WebApplication3.Models
+{
+    public class StaffModel
+    {
+        public StaffModel(Staff staffModel)
+        {
+            id = staffModel.id;
+            name = staffModel.name;
+            phone = staffModel.phone;
+            email = staffModel.email;
+        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
